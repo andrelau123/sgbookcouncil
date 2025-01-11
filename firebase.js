fa387cfc73f786b87af484a4cddd90d2fd9e9739
andre <lautseernandre1@gmail.com>
2025-01-11 12:50:23 +0800

    added firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVUhl5UblUjEeYRdBfN4C2m9pZYPOtPRM",
+  authDomain: "sgbookcouncil.firebaseapp.com",
+  projectId: "sgbookcouncil",
+  storageBucket: "sgbookcouncil.firebasestorage.app",
+  messagingSenderId: "284893267364",
+  appId: "1:284893267364:web:9e65edb8d3bc5ca27aad31",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
