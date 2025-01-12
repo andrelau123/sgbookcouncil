f986f0d0c9a5b7e9fd0f92b9e93fe234758c63c1
hansellohs <hansel.loh.2023@scis.smu.edu.sg>
2025-01-12 17:44:51 +0800

    drafting tasks page

--- src/screens/tasksPage.vue
+++ src/screens/tasksPage.vue
@@ -1,5 +1,22 @@
 <template>
   <h1>tasks page</h1>
+  <br>
+  <div>
+    Charts Visuals Overview
+  </div>
+  <br>
+  <div>
+    Tasks List
+  </div>
+  <br>
+  <div>
+    <button>Create Task</button>
+  </div>
+  <br>
+  <div>
+    <button>Delete Task</button>
+  </div>
+
 </template>
 
 <script>
