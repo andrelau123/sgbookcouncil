9c9c07115c5f56176b17dba84a1cb3ffcd4e7552
andre <lautseernandre1@gmail.com>
2025-01-15 18:30:50 +0800

    updates

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import profilePage from "@/screens/profilePage.vue";
 import aiPage from "@/screens/aiPage.vue";
 import loginPage from "@/screens/loginPage.vue";
 import { auth } from "../../firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 const routes = [
   {
@@ -53,8 +54,24 @@ const router = createRouter({
   },
 });
 
-router.beforeEach((to, from, next) => {
-  const user = auth.currentUser;
+// Add a flag to track auth state initialization
+let isAuthResolved = false;
+
+const waitForAuth = () =>
+  new Promise((resolve) => {
+    if (isAuthResolved) {
+      resolve(auth.currentUser);
+    } else {
+      onAuthStateChanged(auth, (user) => {
+        isAuthResolved = true;
+        resolve(user);
+      });
+    }
+  });
+
+router.beforeEach(async (to, from, next) => {
+  const user = await waitForAuth();
+
   if (to.name === "Login" && user) {
     next({ name: "Home" });
   } else if (to.meta.requiresAuth && !user) {
